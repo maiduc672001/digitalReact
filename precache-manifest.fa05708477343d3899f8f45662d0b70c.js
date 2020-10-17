f78defaaf6b520b5962f563a7d14017a28d77585
ducmai672001 <nguyenduc372001@gmail.com>
2020-10-17 13:30:01 +0700

    Updates

---
+++ precache-manifest.fa05708477343d3899f8f45662d0b70c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8f00952991d637d6a1b5c5b347598a",
+    "url": "/digitalReact/index.html"
+  },
+  {
+    "revision": "286d7bbf365c1ad4f6d1",
+    "url": "/digitalReact/static/css/2.9db35e51.chunk.css"
+  },
+  {
+    "revision": "0cd237d73d07ef795b5e",
+    "url": "/digitalReact/static/css/main.4ca22134.chunk.css"
+  },
+  {
+    "revision": "286d7bbf365c1ad4f6d1",
+    "url": "/digitalReact/static/js/2.c47f640c.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/digitalReact/static/js/2.c47f640c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd237d73d07ef795b5e",
+    "url": "/digitalReact/static/js/main.af92a159.chunk.js"
+  },
+  {
+    "revision": "2b4ec63fb9cda82e783b",
+    "url": "/digitalReact/static/js/runtime-main.49ae03ad.js"
+  }
+]);
